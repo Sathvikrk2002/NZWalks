ccafd4f98c7aca71f79bf4f7fa42edd0e85d3e9f
Aditya Deshmukh <adityadeshmukh7350@gmail.com>
2024-10-23 22:49:19 +0530

    Created Domain models for Walk Project

---
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,19 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+        public Guid DifficultyId { get; set; }
+
+        //Navigation Property - Tells EF that walk will have Difficulty - Defines ONE TO ONE 
+        public Difficulty Difficulty { get; set; }
+        public Guid RegionId { get; set; }
+        //Navigation Property - Tells EF that walk will have Region  - Defines ONE TO ONE 
+        public Region Region { get; set; }
+
+    }
+}
